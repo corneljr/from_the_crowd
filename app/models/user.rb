28797c4b400f83ace5ebce2b3f14e5e1f1c68938
davidhurley <dhurls99@gmail.com>
2014-06-25 19:12:16 -0400

    add articles models and add to suer model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	authenticates_with_sorcery!
 
+	has_many :articles
+	has_many :comments
+
 	validates :password, length: { minimum: 5 }
 	validates :password, confirmation: true
 	validates :password_confirmation, presence: true
