cfbe01cb9bd3206fb3cafb6a40dc69108ccad9a9
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-30 17:49:28 -0400

    updated article index page and fixed user log in bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :require_login
 
+  private 
+  def not_authenticated
+  	redirect_to new_user_session_path, alert: 'You need to login to do this.'
+  end
+
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,14 +1,13 @@
 class UserSessionsController < ApplicationController
-  skip_before_filter :require_login, only: [:destroy]
+  skip_before_filter :require_login, only: [:destroy, :new, :create]
 
   def new
-  	@user = User.new
   end
 
   def create
   	if @user = login(params[:email], params[:password])
-  		redirect_back_or_to(:users, notice: 'Login successful')
-      binding.pry
+      session[:user_id] = @user.id
+  		redirect_to articles_path, notice: 'Login successful'
   	else
   		flash.now[:alert] = 'Login failed'
   		render action: 'new'
