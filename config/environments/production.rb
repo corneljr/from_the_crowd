0e018e1e96ba5ed0917f18df07c100a40095f821
davidhurley <dhurls99@gmail.com>
2014-07-02 00:18:25 -0400

    hide public keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['from_the_crowd'],
-    :access_key_id => ENV['AKIAIHVKXGELZZBE4NDQ'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
