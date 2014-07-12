e99d80c4e9d2daf0366a15c496704aa65de6be04
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-12 16:12:29 -0400

    user profile page styling

--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 	$('.user-stats-link').on('click', function(){
+		$(this).css({'backgroundColor':'black', 'color':'white'})
+		$('.user-about-link').css({'backgroundColor':'white','color':'black'})
 		$('.user-description').hide();
 		$('.user-stats').fadeIn();
 	});
 
 	$('.user-about-link').on('click', function(){
+		$(this).css({'backgroundColor':'black', 'color':'white'})
+		$('.user-stats-link').css({'backgroundColor':'white','color':'black'})
 		$('.user-stats').hide();
 		$('.user-description').fadeIn();
 	});
