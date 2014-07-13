2d69994b2d3aa401b549e9f4cc27a826a479f6e8
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-12 21:41:48 -0400

    added article self destruction

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,7 +9,12 @@ class VotesController < ApplicationController
 			@vote = @article.votes.build(vote_params)
 			@vote.user_id = current_user.id
 			@vote.save
-			redirect_to @article, notice: 'Thanks for rating the article!'
+			if destroy?(@article)
+				@article.destroy
+				redirect_to articles_path, notice: 'Article has been archived'
+			else
+				redirect_to @article, notice: 'Thanks for rating the article!'
+			end
 		end
 	end
 
